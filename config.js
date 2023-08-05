18ff6b83662ebae44ed4f0d959bf8dfd2d9a3117
THEEKSHANA MAX <119984477+Theekshanamax@users.noreply.github.com>
2023-08-05 12:33:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,55 @@
+const fs = require('fs-extra')
+if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/config.env' })
+
+
+//═══════[Required Variables]════════\\
+global.owner = process.env.OWNER_NUMBER.split(",")
+global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
+global.port= process.env.PORT || 5000
+global.email = 'sam@sec.live'
+global.github = 'https://github.com/Theekshanamax/Theekshana-md'
+global.location = 'Sultanpur IN'
+global.gurl = 'https://instagram.com/' // add your username
+global.sudo = process.env.SUDO || '94719892396'
+global.devs = '94762280384';
+global.website = 'https://github.com/Theekshanamax/Theekshana-md' //wa.me/+94719892396
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.ibb.co/vmt8Nnj/20230805-115843.jpg'
+module.exports = {
+  botname: process.env.BOT_NAME || '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨',
+  autosticker: process.env.AUTO_STICKER || 'false',
+  autovoice: process.env.AUTO_VOICE || 'false',
+  botapikey: process.env.BOTCAHX_API || '',
+  alivepic: process.env.ALIVE_IMG || 'https://i.ibb.co/vmt8Nnj/20230805-115843.jpg',
+  alivemsj: process.env.ALIVE_MSJ || '*The bot developer is theekshana* 💙',
+  ownername:process.env.OWNER_NAME || 'Theekshana',
+  sessionName: process.env.SESSION_ID || 'PUT-HERE',
+  author: process.env.PACK_INFO.split(";")[0] || 'author', 
+  auto_read_status : process.env.AUTO_READ_STATUS || 'false',
+  packname: process.env.PACK_INFO.split(";")[1] || 'Name',
+  autoreaction: process.env.AUTO_REACTION || 'off',
+  antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokey',
+  alwaysonline: process.env.ALWAYS_ONLINE || 'false',
+  antifake : process.env.FAKE_COUNTRY_CODE || '',
+  readmessage: process.env.READ_MESSAGE || true,
+  HANDLERS: process.env.PREFIX || ['.'],
+  warncount : process.env.WARN_COUNT || 3,
+  disablepm: process.env.DISABLE_PM || "flase",
+  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'false',
+  antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
+  antilinkaction: process.env.ANTILINK_ACTION || 'remove',
+  BRANCH: 'main',
+  ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || '',
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || 'put-key-here',
+  VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
+  LANG: process.env.THEME|| 'Theekshana-md',
+  WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
+};
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+    delete require.cache[file]
+	require(file)
+})
